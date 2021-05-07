47616d2d2289edfb21f641918e3ddbe7dd9730e8
hakisan99 <sonngo81.99@gmail.com>
2021-05-07 17:47:10 +0700

    BREAKING-CHANGE: delete chart related stuff

--- client/src/pageList.js
+++ client/src/pageList.js
@@ -1,14 +1,9 @@
 import React from 'react'
-import {BsHouse, BsSearch, BsClock, BsBarChart} from 'react-icons/bs'
-import OverallDashboardPage from './components/OverallDashboard'
+import { BsSearch } from 'react-icons/bs'
 import SearchEnginePage from './components/SearchEngine'
-import TimelineViewPage from './components/TimelineView'
-import StaffPerformancePage from './components/StaffPerformance'
+
 const pageList = [
-    {icon: <BsHouse size="24px"/>, text: "Overall Dashboard", path: "/", comp: <OverallDashboardPage/>},
-    {icon: <BsSearch size="24px"/>, text: "Search Engine", path: "/search", comp: <SearchEnginePage/>},
-    {icon: <BsClock size="24px"/>, text: "Timeline View", path: "/timeline", comp: <TimelineViewPage/>},
-    {icon: <BsBarChart size="24px"/>, text: "Staff's Performance", path: "/performance", comp: <StaffPerformancePage/>}
+    {icon: <BsSearch size="24px"/>, text: "Search Engine", path: "/", comp: <SearchEnginePage/>},
 ]
 
 export default pageList
