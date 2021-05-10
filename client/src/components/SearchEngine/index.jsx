38d44f2aee6947b5b9513a7a58a67f4ef7d92d0e
hakisan99 <sonngo81.99@gmail.com>
2021-05-10 11:10:29 +0700

    feat(task-detail): promise to async await

--- client/src/components/SearchEngine/index.jsx
+++ client/src/components/SearchEngine/index.jsx
@@ -1,40 +1,47 @@
-import React, { useEffect, useState } from 'react'
-import styled from "styled-components"
-import List from "./List"
-import Content from './Content'
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import List from './List';
+import Content from './Content';
 
 const Container = styled.div`
-    display: flex;
-    width: 100%;
-    height: 100%;
-` 
+  display: flex;
+  width: 100%;
+  height: 100%;
+`;
 
 const SearchEnginePage = () => {
-    const [selectedId, setSelectedId] = useState()
-    const [selectedItem, setSelectedItem] = useState(null)
-    const [loadDetail, setLoadDetail] = useState(false)
-    useEffect(() => {
-        if (selectedId) {
-            const getDetail = async () => {
-                setLoadDetail(true)
-                fetch('http://172.30.1.213:3600/api/v1/tasks/M04/' + selectedId, {method: 'GET'})
-                    .then((res) => res.json())
-                    .then((result) => {
-                        setSelectedItem(result)
-                        setLoadDetail(false)
-                    })
-                    .catch((err) => console.log(err));
-            }
-            getDetail()
+  const [selectedId, setSelectedId] = useState();
+  const [selectedItem, setSelectedItem] = useState(null);
+  const [loadDetail, setLoadDetail] = useState(false);
+  useEffect(() => {
+    if (selectedId) {
+      const getDetail = async () => {
+        try {
+          setLoadDetail(true);
+          const response = await fetch(
+            'http://172.30.1.213:3600/api/v1/tasks/M04/' + selectedId,
+            { method: 'GET' }
+          );
+          if (!response.ok) {
+            throw new Error('Failed to fetch detail of task ' + selectedId);
+          }
+          const result = await response.json();
+          setSelectedItem(result);
+        } catch (err) {
+          console.error(err);
+        } finally {
+          setLoadDetail(false);
         }
-        
-    }, [selectedId])
-    return (
-        <Container>
-            <List selectedId={selectedId} setSelectedId={setSelectedId} />
-            <Content item={selectedItem} loading={loadDetail}/>
-        </Container>
-    )
-}
+      };
+      getDetail();
+    }
+  }, [selectedId]);
+  return (
+    <Container>
+      <List selectedId={selectedId} setSelectedId={setSelectedId} />
+      <Content item={selectedItem} loading={loadDetail} />
+    </Container>
+  );
+};
 
-export default SearchEnginePage
+export default SearchEnginePage;
